345843d78bbbed54d313ea9a6aed6ea922869c95
Rharison <rharison.abreu@gmail.com>
2025-07-14 19:24:33 -0300

    feat: companies service custom hook

---
+++ src/services/companies/useCompaniesService.ts
@@ -0,0 +1,28 @@
+import type { Asset } from "../../commons/types/assets";
+import type { Company } from "../../commons/types/companies";
+import type { Location } from "../../commons/types/locations";
+import { useHttpClientFactory } from "../useHttpClientFactory";
+
+const BASE_PATH = "/companies";
+
+export const useCompaniesService = () => {
+  const { fetch } = useHttpClientFactory();
+
+  async function getCompanies() {
+    return await fetch<Company[]>(BASE_PATH);
+  }
+
+  async function getLocationsByCompanieId(companyId: string) {
+    return await fetch<Location[]>(`${BASE_PATH}/${companyId}/locations`);
+  }
+
+  async function getAssetsByCompanieId(companyId: string) {
+    return await fetch<Asset[]>(`${BASE_PATH}/${companyId}/locations`);
+  }
+
+  return {
+    getCompanies,
+    getLocationsByCompanieId,
+    getAssetsByCompanieId,
+  };
+};
