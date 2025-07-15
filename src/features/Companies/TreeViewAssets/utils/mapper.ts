ed6ba35461abb7e48855842de37b8d02289e22e5
Rharison <rharison.abreu@gmail.com>
2025-07-15 18:26:20 -0300

    feat: init create util to map data of tree

---
+++ src/features/Companies/TreeViewAssets/utils/mapper.ts
@@ -0,0 +1,8 @@
+import type { TreeItem } from "@commons/types/tree-view-assets";
+import type { Location } from "@commons/types/locations";
+
+export const groupLocationsWithSubLocations = (
+  locations: Location[]
+): TreeItem[] => {
+  return [];
+};
