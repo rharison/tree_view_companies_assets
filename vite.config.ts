655da0d05898539f9f2c253bdfae7c712e875ee6
Rharison <rharison.abreu@gmail.com>
2025-07-15 18:25:35 -0300

    feat: add path alias in conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "src"),
+      "@commons": path.resolve(__dirname, "src/commons"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+    },
+  },
 });
