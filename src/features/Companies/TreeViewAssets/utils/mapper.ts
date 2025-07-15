b0bb4ce855227365d6dd85c4e23f9e7fad9964c2
Rharison <rharison.abreu@gmail.com>
2025-07-15 18:46:16 -0300

    feat: creating mapLocationsWithSubLocationsToTreeItem

--- src/features/Companies/TreeViewAssets/utils/mapper.ts
+++ src/features/Companies/TreeViewAssets/utils/mapper.ts
@@ -1,8 +1,30 @@
 import type { TreeItem } from "@commons/types/tree-view-assets";
 import type { Location } from "@commons/types/locations";
 
-export const groupLocationsWithSubLocations = (
+export const mapLocationsWithSubLocationsToTreeItem = (
   locations: Location[]
 ): TreeItem[] => {
-  return [];
+  return locations
+    .sort((a, b) => {
+      if (!a.parentId && b.parentId) return -1;
+      if (a.parentId && !b.parentId) return 1;
+
+      return 0;
+    })
+    .reduce((acc: TreeItem[], { id, name, parentId }) => {
+      const node: TreeItem = { id, name, children: [] };
+
+      if (!parentId) {
+        acc.push(node);
+      } else {
+        const parent = acc.find((item) => item.id === parentId);
+        if (parent) {
+          parent.children.push(node);
+        } else {
+          acc.push(node);
+        }
+      }
+
+      return acc;
+    }, []);
 };
