69558d156cb1a129f2ea25049c6070a3aa30c967
Rharison <rharison.abreu@gmail.com>
2025-07-16 19:58:23 -0300

    feat: improvement in TreeViewAssetsContext

--- src/features/Companies/TreeViewAssets/contexts/TreeViewAssetsContext.tsx
+++ src/features/Companies/TreeViewAssets/contexts/TreeViewAssetsContext.tsx
@@ -1,7 +1,10 @@
-import { createContext, useContext, useState } from "react";
+import type { Company } from "@src/commons/types/companies";
+import { useCompaniesService } from "@src/services/companies/useCompaniesService";
+import { createContext, useContext, useEffect, useState } from "react";
 
 type TreeViewAssetsContextProps = {
   selectedCompanyId: string | null;
+  companies: Company[];
   handleSelectCompany: (companyId: string) => void;
 };
 
@@ -16,9 +19,31 @@ const TreeViewAssetsContext = createContext<TreeViewAssetsContextProps | null>(
 export function TreeViewAssetsProvider({
   children,
 }: TreeViewAssetsProviderProps) {
+  const [isFetchingCompanies, setIsFetchingCompanies] = useState(true);
+  const [companies, setCompanies] = useState<Company[]>([]);
   const [selectedCompanyId, setSelectedCompanyId] = useState<string | null>(
     null
   );
+
+  const { getCompanies } = useCompaniesService();
+
+  useEffect(() => {
+    loadCompanies();
+  }, []);
+
+  async function loadCompanies() {
+    try {
+      const result = await getCompanies();
+
+      setCompanies(result);
+
+      if (result.length > 0) setSelectedCompanyId(result[0].id);
+    } catch (error) {
+      console.error("Failed to fetch companies:", error);
+    } finally {
+      setIsFetchingCompanies(false);
+    }
+  }
 
   function handleSelectCompany(companyId: string) {
     setSelectedCompanyId(companyId);
@@ -26,7 +51,7 @@ export function TreeViewAssetsProvider({
 
   return (
     <TreeViewAssetsContext.Provider
-      value={{ selectedCompanyId, handleSelectCompany }}
+      value={{ selectedCompanyId, companies, handleSelectCompany }}
     >
       {children}
     </TreeViewAssetsContext.Provider>
