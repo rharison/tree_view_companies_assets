d8e024895a217eb566af4a7edb347b8725f9a8b8
Rharison <rharison.abreu@gmail.com>
2025-07-16 19:58:32 -0300

    feat: improvement in TreeViewAssets page

--- src/features/Companies/TreeViewAssets/index.tsx
+++ src/features/Companies/TreeViewAssets/index.tsx
@@ -1,9 +1,13 @@
+import { Header } from "./components/Header";
+import { PageLayout } from "./components/PageLayout";
 import { TreeViewAssetsProvider } from "./contexts/TreeViewAssetsContext";
 
 export const TreeViewAssets = () => {
   return (
     <TreeViewAssetsProvider>
-      <div>Tree View Assets</div>
+      <PageLayout>
+        <Header />
+      </PageLayout>
     </TreeViewAssetsProvider>
   );
 };
