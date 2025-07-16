58dd09ba82d5c89d455b4dde4badc98d43c652dc
Rharison <rharison.abreu@gmail.com>
2025-07-16 18:47:19 -0300

    feat: improvement in TreeItem type

--- src/commons/types/tree-view-assets.ts
+++ src/commons/types/tree-view-assets.ts
@@ -1,5 +1,19 @@
+import type { AssetStatus, SensorType } from "./assets";
+
 export type TreeItem = {
   id: string;
   name: string;
+  type: TreeItemType;
+  locationId?: string | null;
+  sensorId?: string | null;
+  sensorType?: SensorType | null;
+  status?: AssetStatus | null;
+  gatewayId?: string;
   children: TreeItem[];
 };
+
+export enum TreeItemType {
+  LOCATION = "location",
+  ASSET = "asset",
+  COMPONENT = "component",
+}
