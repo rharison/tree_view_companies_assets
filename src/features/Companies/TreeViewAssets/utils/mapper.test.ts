e46bbde54fde4fcb4bc110c466648891b69e7295
Rharison <rharison.abreu@gmail.com>
2025-07-16 18:47:08 -0300

    feat: improvement in tests of mappers

--- src/features/Companies/TreeViewAssets/utils/mapper.test.ts
+++ src/features/Companies/TreeViewAssets/utils/mapper.test.ts
@@ -1,30 +1,84 @@
+import { AssetStatus, SensorType, type Asset } from "@src/commons/types/assets";
+import {
+  TreeItemType,
+  type TreeItem,
+} from "@src/commons/types/tree-view-assets";
 import { expect, test } from "vitest";
+import { KEY_WITHOUT_LOCATION, mapAssetsTreeItems } from "./mapper";
 
-import { mapLocationsWithSubLocationsToTreeItem } from "./mapper";
-
-test("should map locations with sub-locations to tree items", () => {
-  const locationsMock = [
-    { id: "1", name: "Location 1", parentId: null },
-    { id: "2", name: "Location 2", parentId: "1" },
-    { id: "3", name: "Location 3", parentId: "1" },
-    { id: "4", name: "Location 4", parentId: null },
-  ];
-
-  const result = mapLocationsWithSubLocationsToTreeItem(locationsMock);
-
-  expect(result).toEqual([
+test("when calling mapAssetsTreeItems, should return a map of assets with children by location", () => {
+  const assetsMock: Asset[] = [
     {
       id: "1",
-      name: "Location 1",
-      children: [
-        { id: "2", name: "Location 2", children: [] },
-        { id: "3", name: "Location 3", children: [] },
-      ],
+      name: "Asset 1",
+      parentId: null,
+      locationId: "loc_1",
+      sensorId: null,
+      sensorType: null,
+      status: null,
     },
     {
-      id: "4",
-      name: "Location 4",
-      children: [],
+      id: "2",
+      name: "Asset 2",
+      parentId: "1",
+      locationId: null,
+      sensorId: "sensor_1",
+      sensorType: SensorType.ENERGY,
+      status: AssetStatus.ALERT,
     },
-  ]);
+    {
+      id: "3",
+      name: "Asset 3",
+      parentId: null,
+      locationId: null,
+      sensorId: "sensor_2",
+      sensorType: SensorType.VIBRATION,
+      status: AssetStatus.OPERATING,
+    },
+  ];
+
+  const result = mapAssetsTreeItems(assetsMock);
+
+  console.log("Result:", result);
+  const expected = new Map<string, TreeItem[]>(
+    Object.entries({
+      loc_1: [
+        {
+          id: "1",
+          name: "Asset 1",
+          type: TreeItemType.ASSET,
+          locationId: "loc_1",
+          sensorId: null,
+          sensorType: null,
+          status: null,
+          children: [
+            {
+              id: "2",
+              name: "Asset 2",
+              type: TreeItemType.COMPONENT,
+              locationId: null,
+              sensorId: "sensor_1",
+              sensorType: SensorType.ENERGY,
+              status: AssetStatus.ALERT,
+              children: [],
+            },
+          ],
+        },
+      ],
+      [KEY_WITHOUT_LOCATION]: [
+        {
+          id: "3",
+          name: "Asset 3",
+          locationId: null,
+          type: TreeItemType.COMPONENT,
+          sensorId: "sensor_2",
+          sensorType: SensorType.VIBRATION,
+          status: AssetStatus.OPERATING,
+          children: [],
+        },
+      ],
+    })
+  );
+
+  expect(result).toEqual(expected);
 });
