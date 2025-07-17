067c7d42c7f3ddc496fd856ab61576e01e84187a
Rharison <rharison.abreu@gmail.com>
2025-07-17 11:08:39 -0300

    delete: remove unnecessary component

--- src/features/Companies/TreeViewAssets/components/PageLayout/index.tsx
+++
@@ -1,7 +0,0 @@
-type PageLayoutProps = {
-  children: React.ReactNode;
-};
-
-export const PageLayout = ({ children }: PageLayoutProps) => {
-  return <div className="flex flex-col w-full">{children}</div>;
-};
