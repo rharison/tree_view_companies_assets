9df6169c8cf469692c956fbe28b1a42189d1fc64
Rharison <rharison.abreu@gmail.com>
2025-07-19 12:34:38 -0300

    feat: init render tree items

--- src/features/Companies/Assets/components/TreeViewAssets/componenets/TreeAssets/index.tsx
+++ src/features/Companies/Assets/components/TreeViewAssets/componenets/TreeAssets/index.tsx
@@ -1,9 +1,16 @@
 import type { TreeItem } from "@src/commons/types/tree-view-assets";
+import { TreeAssetsNode } from "./components/TreeAssetsNode";
 
 type TreeAssetsProps = {
   data: TreeItem[];
 };
 
 export const TreeAssets = ({ data }: TreeAssetsProps) => {
-  return <></>;
+  return (
+    <div className="flex flex-col px-1 py-2">
+      {data.map((item) => (
+        <TreeAssetsNode key={item.id} item={item} />
+      ))}
+    </div>
+  );
 };
