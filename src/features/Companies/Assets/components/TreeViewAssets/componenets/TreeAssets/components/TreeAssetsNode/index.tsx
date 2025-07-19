314da0b24d423e3d5c2a433e8bfc4c7e86fb489f
Rharison <rharison.abreu@gmail.com>
2025-07-19 14:49:06 -0300

    feat: evolution of the creation of the asset tree view

--- src/features/Companies/Assets/components/TreeViewAssets/componenets/TreeAssets/components/TreeAssetsNode/index.tsx
+++ src/features/Companies/Assets/components/TreeViewAssets/componenets/TreeAssets/components/TreeAssetsNode/index.tsx
@@ -1,9 +1,37 @@
+import Arrow from "@src/assets/icons/Arrow";
 import type { TreeItem } from "@src/commons/types/tree-view-assets";
+import { resolveIconByTreeItemType } from "../../utils/resolveIconByTreeItemType";
+import { resolveIconFeedbackStatusComponent } from "../../utils/resolveIconFeedbackStatusComponent";
+import { useTreeAssetsNodeController } from "./hooks/useTreeAssetsNodeController";
+import { cn } from "@src/commons/utils/className";
 
 type TreeAssetsNodeProps = {
   item: TreeItem;
+  hasExpandableItems: boolean;
 };
 
-export const TreeAssetsNode = ({ item }: TreeAssetsNodeProps) => {
-  return <span>{item.name}</span>;
+export const TreeAssetsNode = ({
+  item,
+  hasExpandableItems,
+}: TreeAssetsNodeProps) => {
+  const { expandable, open, toggleOpen } = useTreeAssetsNodeController(item);
+  const icon = resolveIconByTreeItemType(item.type);
+  const feedbackIcon = resolveIconFeedbackStatusComponent(item);
+
+  console.log("hasExpandableItems", hasExpandableItems);
+
+  return (
+    <button
+      className={cn(
+        "flex items-center gap-1 cursor-pointer  hover:bg-[#f0f4f7] p-1 rounded",
+        !expandable && hasExpandableItems && "pl-4"
+      )}
+      onClick={toggleOpen}
+    >
+      {expandable && <Arrow up={!open} />}
+      {icon}
+      <span className="text-[#17192D] text-sm">{item.name}</span>
+      {feedbackIcon}
+    </button>
+  );
 };
--- src/features/Companies/Assets/components/TreeViewAssets/componenets/TreeAssets/index.tsx
+++ src/features/Companies/Assets/components/TreeViewAssets/componenets/TreeAssets/index.tsx
@@ -1,15 +1,22 @@
 import type { TreeItem } from "@src/commons/types/tree-view-assets";
 import { TreeAssetsNode } from "./components/TreeAssetsNode";
+import { useTreeAssetsController } from "./hooks/useTreeAssetsController";
 
 type TreeAssetsProps = {
   data: TreeItem[];
 };
 
 export const TreeAssets = ({ data }: TreeAssetsProps) => {
+  const { hasExpandableItems } = useTreeAssetsController(data);
+
   return (
-    <div className="flex flex-col px-1 py-2">
+    <div className="flex flex-col px-1 py-2 gap-1">
       {data.map((item) => (
-        <TreeAssetsNode key={item.id} item={item} />
+        <TreeAssetsNode
+          key={item.id}
+          item={item}
+          hasExpandableItems={hasExpandableItems}
+        />
       ))}
     </div>
   );
