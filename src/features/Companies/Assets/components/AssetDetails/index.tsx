4988ae664b959e124d2b73d555441a24de09230b
Rharison <rharison.abreu@gmail.com>
2025-07-19 11:39:45 -0300

    feat: improvement in TreeViewAssets page

--- src/features/Companies/Assets/components/AssetDetails/index.tsx
+++ src/features/Companies/Assets/components/AssetDetails/index.tsx
@@ -1,5 +1,11 @@
 export const AssetDetails = () => {
   return (
-    <div className="flex w-full h-full border border-[#D8DFE6] rounded-[2px]"></div>
+    <div className="flex flex-col w-full h-full border border-[#D8DFE6] rounded-[2px]">
+      <header className="h-[45px] border-b border-[#D8DFE6] flex items-center px-4 py-3.5">
+        <span className="text-[#24292F] font-semibold">
+          MOTORS H12D - Stage 3
+        </span>
+      </header>
+    </div>
   );
 };
--- src/features/Companies/Assets/components/TreeViewAssets/index.tsx
+++ src/features/Companies/Assets/components/TreeViewAssets/index.tsx
@@ -1,5 +1,17 @@
+import Search from "@src/assets/icons/Search";
+
 export const TreeViewAssets = () => {
   return (
-    <div className="flex w-full h-full border border-[#D8DFE6] rounded-[2px]"></div>
+    <div className="flex flex-col w-[40%] h-full border border-[#D8DFE6] rounded-[2px]">
+      <div className="relative w-full">
+        <input
+          id="search-input"
+          type="text"
+          placeholder="Buscar Ativo ou Local"
+          className="w-full h-[45px] p-2 border-b border-[#D8DFE6] focus:outline-none placeholder-[#C1C9D2] placeholder:text-sm px-3"
+        />
+        <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none" />
+      </div>
+    </div>
   );
 };
