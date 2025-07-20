e5e025c99c4197921aa1c1ed186db08fcc33db77
Rharison <rharison.abreu@gmail.com>
2025-07-20 14:17:06 -0300

    feat: add companies loading control in Header

--- src/features/Companies/pages/Assets/components/Header/index.tsx
+++ src/features/Companies/pages/Assets/components/Header/index.tsx
@@ -2,28 +2,37 @@ import Gold from "@src/assets/icons/Gold";
 import LogoTractian from "@src/assets/images/LogoTractian";
 import { cn } from "@src/commons/utils/className";
 import { useCompanyContext } from "../../../../contexts/CompanyContext";
+import { CompaniesButtonsSkeleton } from "./components/CompaniesButtonsSkeleton";
 
 export const Header = () => {
-  const { companies, selectedCompanyId, handleSelectCompany } =
-    useCompanyContext();
+  const {
+    companies,
+    selectedCompanyId,
+    isFetchingCompanies,
+    handleSelectCompany,
+  } = useCompanyContext();
 
   return (
     <nav className="bg-[#17192D] flex h-12 justify-between items-center py-3 px-4">
       <LogoTractian />
       <div className="flex gap-[10px]">
-        {companies.map((company) => (
-          <button
-            key={company.id}
-            className={cn(
-              "text-white h-6 py-1 px-2 text-xs rounded-[2px] flex items-center gap-2 cursor-pointer",
-              company.id === selectedCompanyId ? "bg-[#2188FF]" : "bg-[#023B78]"
-            )}
-            onClick={() => handleSelectCompany(company.id)}
-          >
-            <Gold />
-            {company.name}
-          </button>
-        ))}
+        {isFetchingCompanies && <CompaniesButtonsSkeleton />}
+        {!isFetchingCompanies &&
+          companies.map((company) => (
+            <button
+              key={company.id}
+              className={cn(
+                "text-white h-6 py-1 px-2 text-xs rounded-[2px] flex items-center gap-2 cursor-pointer",
+                company.id === selectedCompanyId
+                  ? "bg-[#2188FF]"
+                  : "bg-[#023B78]"
+              )}
+              onClick={() => handleSelectCompany(company.id)}
+            >
+              <Gold />
+              {company.name}
+            </button>
+          ))}
       </div>
     </nav>
   );
